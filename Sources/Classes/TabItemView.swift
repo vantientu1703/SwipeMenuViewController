4fa46a6a294600ec20ebd0bb104e3e7a9529ac99
swiftlab <vantientu@vantientu.local>
2025-05-15 10:22:40 +0700

    update

--- Sources/Classes/TabItemView.swift
+++ Sources/Classes/TabItemView.swift
@@ -2,7 +2,7 @@ import UIKit
 
 final class TabItemView: UIView {
 
-    private(set) var titleLabel: UILabel = UILabel()
+    public var titleLabel: UILabel = UILabel()
 
     public var textColor: UIColor = UIColor(red: 140/255, green: 140/255, blue: 140/255, alpha: 1.0)
     public var selectedTextColor: UIColor = .white
